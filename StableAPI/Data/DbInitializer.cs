fdd9d743d54a350dac78de10f161a7f8885a7e72
i34tbu <baptiste.bab@gmail.com>
2020-06-17 13:23:43 -0400

    Started to seed database Corrections to model

--- StableAPI/Data/DbInitializer.cs
+++ StableAPI/Data/DbInitializer.cs
@@ -9,14 +9,56 @@ namespace StableAPI.Data
         {
             context.Database.EnsureCreated();
 
-            if (context.Stables.Any())
+            if (context.Persons.Any())
             {
                 return;   // DB has been seeded
             }
 
+            var roles = new Role[]
+            {
+                new Role{Name = "default"},
+                new Role{Name = "admin"},
+                new Role{Name = "groom"},
+                new Role{Name = "secretary"},
+            };
+            foreach (var role in roles)
+            {
+                context.Roles.Add(role);
+            }
+
+            context.SaveChanges();
+
+            var persons = new Person[]
+            {
+                new Person {Name = "Doe", Surname = "John"},
+                new Person {Name = "Martin", Surname = "Pierre"},
+                new Person {Name = "Muller", Surname = "Hans"},
+                new Person {Name = "West", Surname = "Malik"},
+            };
+            foreach (var person in persons)
+            {
+                context.Persons.Add(person);
+            }
+
+            context.SaveChanges();
+
+            var users = new User[]
+            {
+                new User {Name = "Boss", Surname = "E", UserName = "admin", PassHash = "admin"},
+                new User {Name = "Secretary", Surname = "F", UserName = "secretary", PassHash = "secretary"},
+                new User {Name = "Groom", Surname = "G", UserName = "groom", PassHash = "groom"},
+                new User {Name = "Doe", Surname = "John", UserName = "jdoe", PassHash = "password"}
+            };
+            foreach (var user in users)
+            {
+                context.Users.Add(user);
+            }
+
+            context.SaveChanges();
+
             var stables = new Stable[]
             {
-                new Stable()
+                new Stable{BossID = 5},
             };
             foreach (var stable in stables)
             {
--- StableAPI/Models/Person.cs
+++ StableAPI/Models/Person.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StableAPI.Models
 {
     public class Person
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public string Surname { get; set; }
         
         public ICollection<Horse> Horses { get; set; }
         public ICollection<Membership> Memberships { get; set; }
-        public User User { get; set; }
     }
 }
