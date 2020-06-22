ae86a1de2894f28dd1bbe4e49501b612ed8854ef
Damien Galochet <damien.galochet@gmail.com>
2020-06-22 19:16:59 -0400

    Home and login started

--- stable_client/src/App.vue
+++ stable_client/src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar class="container" toggleable="lg" type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item to="/home">Home</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <router-view class="container" v-bind:rights="rights" v-bind:states="states" v-on:login="login">
+    </router-view>
+    <b-toast id="error_toast" variant="danger" solid toaster="b-toaster-top-center">
+      <template v-slot:toast-title>
+        <div class="d-flex flex-grow-1 align-items-baseline">
+          <b-img blank blank-color="#ff5555" class="mr-2 rounded" width="12" height="12" ></b-img>
+          <strong class="mr-auto">{{ toast.title }}</strong>
+        </div>
+      </template>
+      {{ toast.body }}
+    </b-toast>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+    },
+    data : function() {
+      return {
+        rights : {
+          isConnected : false,
+          isSecretary : false,
+          isGroom : false,
+          isBoss : false,
+          isAdmin : false
+        },
+        baseUrl : 'http://localhost:5000/',
+        states : {
+          login : {
+            running : false,
+            error : false
+          }
+        },
+        toast : {
+          title : '',
+          body  : ''
+        }
+      }
+    },
+    methods : {
+      login : function (loginData) {
+        let that = this;
+        that.states.login.running = true
+        axios.post(this.baseUrl + 'login', loginData, { withCredentials: true })
+        .then()
+        .catch(error => {
+          that.toast.title = 'Error during login';
+          that.toast.body = error.response ? error.response : error.message;
+          that.$bvToast.show('error_toast');
+          that.states.login.running = false;
+        })
+
+      },
+      getRights : function () {
+        let that = this;
+        axios.get(this.baseUrl + 'rights', { withCredentials: true })
+        .then(function (response) {
+          that.rights = response.data;
+        })
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  body {
+    background-color: #343a40 !important;
+    font-family: 'roboto', sans-serif;
+  }
+  .nav-link {
+    &.router-link-active {
+      border-bottom: 1px solid;
+    }
+  }
 </style>
---
+++ stable_client/src/components/Home.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="pt-5">
+        <h1 class="text-center">Welcome to stable</h1>
+        <div v-if="!rights.isConnected" >
+            <h2 class="text-center pt-5 pb-3">Please, login</h2>
+            <div class="text-center">
+                <b-row class="pb-2">
+                    <b-col lg="3"></b-col>
+                    <b-col cols="3" lg="2" class="">
+                        <label for="username" class="m-0">Username</label>
+                    </b-col>
+                    <b-col cols="9" lg="4" class="">
+                        <b-form-input id="username" size="sm" type="text" trim></b-form-input>
+                    </b-col>
+                    <b-col lg="3"></b-col>
+                </b-row>
+                <b-row>
+                    <b-col lg="3"></b-col>
+                    <b-col cols="3" lg="2" class="">
+                        <label for="password" class="m-0">Password</label>
+                    </b-col>
+                    <b-col cols="9" lg="4" class="">
+                        <b-form-input id="password" size="sm" type="password"></b-form-input>
+                    </b-col>
+                    <b-col lg="3"></b-col>
+                </b-row>
+                <b-row>
+                    <b-col lg="4"></b-col>
+                    <b-col cols="12" lg="4" class="pt-2">
+                        <b-btn
+                                v-on:click="$emit('login', login)"
+                                :disabled="states.login.running"
+                                variant="secondary"
+                                class="w-100"
+                        >
+                            <b-spinner small variant="light" label="loading" v-if="states.login.running"></b-spinner>
+                            <span v-else>Login</span>
+                        </b-btn>
+                    </b-col>
+                    <b-col lg="4"></b-col>
+                </b-row>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        props: {
+            rights : Object,
+            states : Object
+        },
+        data: function () {
+            return {
+                login : {
+                    username : '',
+                    password : ''
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    * {
+        color: rgba(255, 255, 255, 0.8);
+    }
+    .rounded-top-left {
+        border-top-left-radius: .25rem !important;
+    }
+    .rounded-top-right {
+        border-top-right-radius: .25rem !important;
+    }
+    .rounded-bottom-right {
+        border-bottom-right-radius: .25rem !important;
+    }
+    .rounded-bottom-left {
+        border-bottom-left-radius: .25rem !important;
+    }
+    .form-control {
+        color: rgba(255, 255, 255, 0.8) !important;
+        background-color: rgba(255, 255, 255, 0.2) !important;
+    }
+</style>
