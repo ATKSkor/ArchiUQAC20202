9943297922a9958325cbbd6a987143e17ee4baf1
ATKSkor <37736143+ATKSkor@users.noreply.github.com>
2020-06-25 14:19:03 +0200

    Ajout request API Person

---
+++ StableAPI/Models/Dto/PersonDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+
+namespace StableAPI.Models.Dto
+{
+    public class PersonDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public virtual ICollection<int> HorsesIDs { get; set; }
+        public ICollection<int> MembershipsIDs { get; set; }
+    }
+}
